373b08b570aa8b5d0d100a5d51508927ef41cb5c
Alvaro <aeromatico@gmail.com>
2019-03-03 14:34:53 +0000

    Updating Mailchimp

--- themes/master/assets/js/main.js
+++ themes/master/assets/js/main.js
@@ -463,10 +463,3 @@ Author : themelooks.com
     });
 })(jQuery);
 
-        /* -------------------------------------------------------------------------*
-         * MAILCHIMP
-         * -------------------------------------------------------------------------*/
-
-
-
-<script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/d1b3449cedbb183c24cad61ce/76c223288ef00160b93d47742.js");</script>
